c89a7efa30d4a0d3e7f49adab712d68fb788c3da
punitpalial <punit.palial@gmail.com>
2025-02-12 16:27:55 +0530

    Extension Complete

--- server.js
+++ server.js
@@ -31,12 +31,19 @@ app.post("/selectedTextExplanation", async (req, res) => {
     const { mode } = req.body;
     const { selectedText } = req.body;
     const prompt = mode + selectedText;
-    const result = await model.generateContent(prompt);
-    const response = result.response.text();
+    // const result = await model.generateContent(prompt);
+    // const response = result.response.text();
 
-    await addToHistory(selectedText, response);
+    const result = await model.generateContentStream(prompt);
 
-    res.json({ explanation: response });
+    for await (const chunk of result.stream) {
+      const chunkText = chunk.text();
+      res.json({ explanation: chunkText });
+    }
+
+    // await addToHistory(selectedText, response);
+
+    res.json({ explanation: result.stream });
   } catch (error) {
     console.log("Error in getting the explanation of the selected text", error);
   }
